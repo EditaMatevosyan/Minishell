134193c61efe327db975c1b9a3061441c731fe7f
Rosie <margaryanrosie@gmail.com>
2025-12-06 15:24:47 +0400

    leak fixed

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: rosie <rosie@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/23 16:26:34 by edmatevo          #+#    #+#             */
-/*   Updated: 2025/12/06 13:36:28 by rosie            ###   ########.fr       */
+/*   Updated: 2025/12/06 15:11:20 by rosie            ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -69,7 +69,7 @@ static void process_input(t_minishell *ms, char *input)
         free_tokens(&ms->tokens);
         return;
     }
-    cmds = parse_tokens(ms->tokens, ms->env);       //this is the head of the liked list
+    cmds = parse_tokens(ms->tokens, ms->env);       //this is the head of the linked list
     if (!cmds)
     {
         free_tokens(&ms->tokens);
@@ -83,22 +83,6 @@ static void process_input(t_minishell *ms, char *input)
     free_cmd_list(&cmds);
     free_tokens(&ms->tokens);
 }
-
-// void print_cmd_list(t_cmd *cmd)
-// {
-// 	int i = 1;
-//     while (cmd)
-//     {
-//         printf("=== CMD %d ===\n", i++);
-//         for (int j = 0; cmd->argv && cmd->argv[j]; j++)
-//             printf("argv[%d]: %s\n", j, cmd->argv[j]);
-//         if (cmd->infile)
-//             printf("infile: %s\n", cmd->infile);
-//         if (cmd->outfile)
-//             printf("outfile: %s (append=%d)\n", cmd->outfile, cmd->append);
-//         cmd = cmd->next;
-//     }
-// }
 
 void minishell_loop(t_minishell *ms)
 {
